aa7cf3fd6b4db595cc5abca703afe3d7d8dcee44
Onuraslann <theeaslann1@gmail.com>
2021-09-17 11:04:24 +0300

    Add project files.

---
+++ Entities/Concrete/Brand.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+   public class Brand: IEntity
+    {
+
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+
+
+
+    }
+}
